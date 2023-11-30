62515927be8b1f24366815346da3a5d6b9e64a4d
Elina Kamaeva <71763367+elinkamaeva@users.noreply.github.com>
2023-11-30 12:30:02 +0300

    Delete create_index.py

--- create_index.py
+++
@@ -1,6 +0,0 @@
-from search import MySearcher
-
-if __name__ == "__main__":
-    searcher = MySearcher(directory='indexdir')
-    searcher.create_index(filepath='./templates/lessons')
-    print("Индекс успешно создан.")
